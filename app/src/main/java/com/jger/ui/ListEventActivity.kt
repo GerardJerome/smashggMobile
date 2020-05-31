a442d049682779f55acba6864c25a7e1cb4ca720
kazors <jerome.gerard02@outlook.fr>
2020-06-01 00:40:48 +0200

    mis en place de la cardview

--- app/src/main/java/com/jger/ui/ListEventActivity.kt
+++ app/src/main/java/com/jger/ui/ListEventActivity.kt
@@ -2,9 +2,11 @@ package com.jger.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.jger.R
 import com.jger.transferClass.EventTransfer
 import com.jger.ui.adapter.ListEventAdapter
+import kotlinx.android.synthetic.main.activity_lis_event.*
 
 class ListEventActivity : AppCompatActivity() {
 
@@ -12,5 +14,7 @@ class ListEventActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lis_event)
         val adapter = ListEventAdapter(EventTransfer.listEvents)
+        EventRecyclerView.adapter=adapter
+        EventRecyclerView.layoutManager=LinearLayoutManager(this)
     }
 }
--- app/src/main/java/com/jger/ui/adapter/ListEventAdapter.kt
+++ app/src/main/java/com/jger/ui/adapter/ListEventAdapter.kt
@@ -3,12 +3,12 @@ package com.jger.ui.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.TournamentQuery
 import com.jger.R
 
-class ListEventAdapter(listEvents: List<TournamentQuery.Event?>?) : RecyclerView.Adapter<ListEventAdapter.EventViewHolder>() {
-lateinit var  listEvent : List<TournamentQuery.Event>
+class ListEventAdapter(val listEvents: List<TournamentQuery.Event?>?) : RecyclerView.Adapter<ListEventAdapter.EventViewHolder>() {
 
     class EventViewHolder(v : View) : RecyclerView.ViewHolder(v){
 
@@ -20,10 +20,10 @@ lateinit var  listEvent : List<TournamentQuery.Event>
     }
 
     override fun getItemCount(): Int {
-        return listEvent.size
+        return listEvents!!.size
     }
 
     override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
-        holder.
+        holder.itemView.findViewById<TextView>(R.id.event_name_txt).text= listEvents!![position]!!.name
     }
 }

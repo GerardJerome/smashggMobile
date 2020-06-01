ab5728589acb52af24887ac36397e79d5f0f0b32
kazors <jerome.gerard02@outlook.fr>
2020-06-01 23:56:24 +0200

    ajout picasso+image dans les cardview

--- app/src/main/java/com/jger/ui/adapter/ListEventAdapter.kt
+++ app/src/main/java/com/jger/ui/adapter/ListEventAdapter.kt
@@ -3,10 +3,12 @@ package com.jger.ui.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.TournamentQuery
 import com.jger.R
+import com.squareup.picasso.Picasso
 
 class ListEventAdapter(val listEvents: List<TournamentQuery.Event?>?) : RecyclerView.Adapter<ListEventAdapter.EventViewHolder>() {
 
@@ -27,6 +29,8 @@ class ListEventAdapter(val listEvents: List<TournamentQuery.Event?>?) : Recycler
         holder.itemView.findViewById<TextView>(R.id.event_name_txt).text= listEvents!![position]!!.name
         holder.itemView.findViewById<TextView>(R.id.nombre_entrant_txt).text =
             """${listEvents!![position]!!.numEntrants.toString()} participant(s)"""
-        holder.itemView.findViewById<TextView>(R.id.start_at_txt).text = listEvents!![position]!!.startAt.
+        holder.itemView.findViewById<TextView>(R.id.start_at_txt).text = listEvents!![position]!!.startAt.toString()
+        Picasso.get().load(listEvents!![position]!!.videogame!!.images!![0]!!.url)
+            .into(holder.itemView.findViewById<ImageView>(R.id.game_image))
     }
 }

abe463d0ad3815f63421d2f34970a40eac01aca9
kazors <jerome.gerard02@outlook.fr>
2020-06-05 18:10:35 +0200

    liaison TournamentActivity+EventActivity

--- app/src/main/java/com/jger/ui/adapter/SearchViewCustomAdapter.kt
+++ app/src/main/java/com/jger/ui/adapter/SearchViewCustomAdapter.kt
@@ -1,6 +1,8 @@
 package com.jger.ui.adapter
 
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,9 +10,16 @@ import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
 import com.example.TempQuery
+import com.example.TournamentQuery
 import com.jger.R
+import com.jger.transferClass.EventTransfer
+import com.jger.ui.ListEventActivity
 import com.jger.ui.MainActivity
+import com.jger.util.ApolloUtil
 import com.squareup.picasso.Picasso
 import java.text.SimpleDateFormat
 import java.util.*
@@ -41,6 +50,24 @@ class SearchViewCustomAdapter(val context : Context, var listTournament: List<Te
         if(listTournament!![position]!!.images!!.isNotEmpty()) {
             Picasso.get().load(listTournament!![position]!!.images!![0]!!.url).resize(200, 400)
                 .into(holder.itemView.findViewById<ImageView>(R.id.tournament_image))
+        }
+        holder.itemView.setOnClickListener{
+            ApolloUtil.apolloClient
+                .query(
+                    TournamentQuery(listTournament!![position]!!.slug!!)
+                )
+                .requestHeaders(ApolloUtil.clientHeader)
+                .enqueue(object : ApolloCall.Callback<TournamentQuery.Data>() {
+                    override fun onFailure(e: ApolloException) {
+                        Log.d("jerom", e.toString())
+                    }
+
+                    override fun onResponse(response: Response<TournamentQuery.Data>) {
+                        EventTransfer.listEvents=response.data!!.tournament!!.events!!
+                        it.context.startActivity(Intent(it.context,ListEventActivity::class.java))
+                    }
+
+                })
         }
 
     }
